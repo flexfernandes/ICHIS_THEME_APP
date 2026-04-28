2c013ce7ac1ccdb0d4a10fa46bc80397b71ab48b
Carlos Fernandes <flexfernandes@gmail.com>
2026-04-28 19:29:21 -0300

    Atualização completa do tema

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="ichis",
-    version="0.0.2",
-    description="App de tema visual ICHIS para ERPNext/Frappe v16",
+    version="0.0.1",
+    description="Tema visual genérico inspirado no Aura para Frappe/ERPNext v16",
     author="GREENFARMS",
     author_email="contato@greenfarms.com.br",
-    packages=find_packages(),
+    packages=find_packages(include=["ichis", "ichis.*"]),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[],
 )
